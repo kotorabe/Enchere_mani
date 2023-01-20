531d5f03f3d2faf1a8f4f5925864cefd85447e25
kotorabe <kotorajao@gmail.com>
2023-01-20 09:40:40 +0300

    sds

--- src/main/java/com/example/enchere/Base/Connexion.java
+++ src/main/java/com/example/enchere/Base/Connexion.java
@@ -5,9 +5,9 @@ import java.sql.DriverManager;
 import java.sql.SQLException;
 
 public class Connexion {
-	private static String url = "jdbc:postgresql://containers-us-west-73.railway.app:7117/railway";
+	private static String url = "jdbc:postgresql://containers-us-west-185.railway.app:5889/railway";
     private static String user="postgres";
-    private static String passwd = "VD0XBBer54mRfm5WwIrw";
+    private static String passwd = "NxPX0hpksmxrlYaTnqg5";
 	private static Connection connect;
 	public static Connection setConnect() throws SQLException {
 		if (connect == null) {
